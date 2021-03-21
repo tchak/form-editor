56ceb0357cd5e81d491a73d142742e0168a73805
Paul Chavard <github@paul.chavard.net>
2021-03-21 12:31:38 +0100

    Improuve header buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  HiOutlineEye,
+  HiOutlineArrowNarrowLeft,
+  HiOutlineRefresh,
+} from 'react-icons/hi';
 
 export function Header({
   preview,
@@ -13,20 +18,36 @@ export function Header({
     <div className="md:flex md:items-center md:justify-between mt-2">
       <div className="flex-1 min-w-0"></div>
       <div className="mt-4 flex md:mt-0 md:ml-4">
-        <button
-          type="button"
-          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-          onClick={onPreview}
-        >
-          {preview ? 'Edit' : 'Preview'}
-        </button>
-        <button
-          type="button"
-          className="ml-3 inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          onClick={onReset}
-        >
-          Reset
-        </button>
+        {preview && (
+          <button
+            type="button"
+            className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={onPreview}
+          >
+            <HiOutlineArrowNarrowLeft className="-ml-0.5 mr-2 h-4 w-4" />
+            Back to editor
+          </button>
+        )}
+        {!preview && (
+          <button
+            type="button"
+            className="inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            onClick={onPreview}
+          >
+            <HiOutlineEye className="-ml-0.5 mr-2 h-4 w-4" />
+            Preview
+          </button>
+        )}
+        {!preview && (
+          <button
+            type="button"
+            className="ml-3 inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={onReset}
+          >
+            <HiOutlineRefresh className="-ml-0.5 mr-2 h-4 w-4" />
+            Reset
+          </button>
+        )}
       </div>
     </div>
   );
